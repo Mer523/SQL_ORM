b13243de91c0787bfaac671a4149b533b2b26916
Mer523 <125193348+Mer523@users.noreply.github.com>
2023-07-09 20:53:50 +0300

    Update models.py
    +

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-import sqlalchemy as sqa
+import sqlalchemy as sq
 from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
@@ -7,56 +7,74 @@ Base = declarative_base()
 class Publisher(Base):
     __tablename__ = "publisher"
 
-    id = sqa.Column(sqa.Integer, primary_key=True)
-    name = sqa.Column(sqa.String(length=40), nullable=False, unique=True)
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=40), unique=True)
+
+    book = relationship("Book", back_populates="pub")
 
     def __str__(self):
-        return f"Издатель {self.id}: {self.name}"
+        return f'{self.id} | {self.name}'
 
 
 class Book(Base):
     __tablename__ = "book"
 
-    id = sqa.Column(sqa.Integer, primary_key=True)
-    title = sqa.Column(sqa.String(length=40), nullable=False)
-    id_publisher = sqa.Column(sqa.Integer, sqa.ForeignKey("publisher.id"), nullable=False)
+    id = sq.Column(sq.Integer, primary_key=True)
+    title = sq.Column(sq.Text, nullable=False)
+    publisher_id = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
 
-    publisher = relationship(Publisher, backref="book")
-
-
-class Shop(Base):
-    __tablename__ = "shop"
-
-    id = sqa.Column(sqa.Integer, primary_key=True)
-    name = sqa.Column(sqa.String(length=40), nullable=False, unique=True)
+    pub = relationship("Publisher", back_populates="book")
+    stock = relationship("Stock", back_populates="book_stock")
 
     def __str__(self):
-        return f"Магазин {self.id}: {self.name}"
+        return f'{self.id} | {self.title} | {self.publisher_id}'
 
 
 class Stock(Base):
     __tablename__ = "stock"
 
-    id = sqa.Column(sqa.Integer, primary_key=True)
-    id_book = sqa.Column(sqa.Integer, sqa.ForeignKey("book.id"), nullable=False)
-    id_shop = sqa.Column(sqa.Integer, sqa.ForeignKey("shop.id"), nullable=False)
-    count = sqa.Column(sqa.Integer, nullable=False)
+    id = sq.Column(sq.Integer, primary_key=True)
+    book_id = sq.Column(sq.Integer, sq.ForeignKey("book.id"), nullable=False)
+    shop_id = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False)
+    count = sq.Column(sq.Integer, nullable=False)
 
-    book = relationship(Book, backref="stock")
-    shop = relationship(Shop, backref="stock")
+    book_stock = relationship("Book", back_populates="stock")
+
+    stock_shop = relationship("Shop", back_populates="shop")
+
+    stock_sale = relationship("Sale", back_populates="sale")
+
+    def __str__(self):
+        return f'{self.id} | {self.title} | {self.shop_id} | {self.count}'
+
+
+class Shop(Base):
+    __tablename__ = "shop"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=40), unique=True)
+
+    shop = relationship("Stock", back_populates="stock_shop")
+
+    def __str__(self):
+        return f'{self.id} | {self.name}'
 
 
 class Sale(Base):
     __tablename__ = "sale"
 
-    id = sqa.Column(sqa.Integer, primary_key=True)
-    price = sqa.Column(sqa.Integer, nullable=False)
-    date_sale = sqa.Column(sqa.Date, nullable=False)
-    id_stock = sqa.Column(sqa.Integer, sqa.ForeignKey("stock.id"), nullable=False)
-    count = sqa.Column(sqa.Integer, nullable=False)
+    id = sq.Column(sq.Integer, primary_key=True)
+    price = sq.Column(sq.Numeric, nullable=False)
+    date_sale = sq.Column(sq.Date, nullable=False)
+    stock_id = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
+    count = sq.Column(sq.Integer, nullable=False)
 
-    stock = relationship(Stock, backref="sale")
+    sale = relationship("Stock", back_populates="stock_sale")
+
+    def __str__(self):
+        return f'{self.id} | {self.price} | {self.date_sale} | {self.stock_id} | {self.count}'
 
 
 def create_tables(engine):
+    Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
